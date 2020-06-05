4b27d2c705d86fbbe9ecfaea6f41ee04d4857178
Mujoung-Kim <lklkk2002@naver.com>
2020-06-05 17:55:36 +0900

    Initial commit

---
+++ app/src/main/java/company/domain/retrofitstudyapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package company.domain.retrofitstudyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
